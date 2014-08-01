df798081918e43640dc46e83bf89108b0a385cb3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-08-01 12:17:24 +0200

    Removed empty add function and updated copy in test

--- app/index.js
+++ app/index.js
@@ -60,8 +60,6 @@ var PropertyTypeGenerator = yeoman.generators.Base.extend({
     }
   },
 
-  app: function () {},
-
   projectfiles: function () {
     this.copy('editorconfig', '.editorconfig');
     this.template('class-property-name.php', 'class-property-' + this.propertyName.toLowerCase() + '.php');
