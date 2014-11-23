d0e6784e2449bf57b9821929d4aebd65862305e5
Fredrik Forsmo <frozzare@users.noreply.github.com>
2014-11-23 22:10:41 +0100

    Update class-papi-property-name.php

--- app/templates/class-papi-property-name.php
+++ app/templates/class-papi-property-name.php
@@ -18,7 +18,9 @@ class Papi_Property_<%= propertyName %> extends Papi_Property {
 	 * @var int
 	 * @since 1.0.0
 	 */
+
 	public $default_value = '';
+
 	/**
 	 * Get default settings.
 	 *
@@ -26,58 +28,59 @@ class Papi_Property_<%= propertyName %> extends Papi_Property {
 	 *
 	 * @return array
 	 */
+
 	public function get_default_settings() {
 		return array();
 	}
 
-  /**
-   * Generate the HTML for the property.
-   *
-   * @since 1.0.0
-   */
+	/**
+	 * Generate the HTML for the property.
+	 *
+	 * @since 1.0.0
+	 */
 
-  public function html () {
-    // Property options
-    $options = $this->get_options();
+	public function html () {
+		// Property options
+	$options = $this->get_options();
 
-    // Database value.
-    $value = $this->get_value();
-    ?>
-    <input type="text" value="<?php echo $value; ?>" name="<?php echo $options->slug; ?>"/>
-    <?php
-  }
+	// Database value.
+	$value = $this->get_value();
+	?>
+	<input type="text" value="<?php echo $value; ?>" name="<?php echo $options->slug; ?>"/>
+	<?php
+	}
 
-  /**
-   * Format the value of the property before we output it to the application.
-   *
-   * @param mixed $value
-   * @param int $post_id
-   *
-   * @since 1.0.0
-   *
-   * @return mixed
-   */
+	/**
+	* Format the value of the property before we output it to the application.
+	*
+	* @param mixed $value
+	* @param int $post_id
+	*
+	* @since 1.0.0
+	*
+	* @return mixed
+	*/
 
-   // This function is not required since it does this in the base class.
+	// This function is not required since it does this in the base class.
 
-   // public function format_value ($value, $post_id) {
-   //  return $value;
-   // }
+	// public function format_value ($value, $post_id) {
+	//	return $value;
+	// }
 
-   /**
-    * Update the value of the property before we save it to the database.
-    *
-    * @param mixed $value
-    * @param int $post_id
-    *
-    * @since 1.0.0
-    *
-    * @return mixed
-    */
+	/**
+	* Update the value of the property before we save it to the database.
+	*
+	* @param mixed $value
+	* @param int $post_id
+	*
+	* @since 1.0.0
+	*
+	* @return mixed
+	*/
 
-    // This function is not required since it does this in the base class.
+	// This function is not required since it does this in the base class.
 
-    // public function update_value ($value, $post_id) {
-    //  return $value;
-    // }
+	// public function update_value ($value, $post_id) {
+	//	return $value;
+	// }
 }
